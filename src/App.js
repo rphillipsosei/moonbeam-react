d99673e47a1fca6bdce8685deb96ab7cf15131fd
Rhoda P <rhodaphillipsosei@gmail.com>
2022-06-07 19:15:57 -0400

    unlink apply pg from react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Navigation from "./components/Navigation.jsx"
-import styled from "styled-components";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Services from "./Services.jsx"
-import {useState} from 'react'
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import Services from "./pages/Services.jsx"
 import Shop from "./pages/Shop.jsx"
 import About from "./pages/About.jsx"
 import Contact from "./pages/Contact.jsx"
-import Apply from "./pages/Apply.jsx"
 import Careers from "./pages/Careers.jsx"
 
 
@@ -25,7 +20,7 @@ function App() {
 <Route path="/about" element={<About/>}/>
 <Route path="/careers" element={<Careers/>}/>
 <Route path="/contact" element={<Contact/>}/>
-<Route path="/apply" element={<Apply/>}/>
+{/* <Route path="/apply" element={<Apply/>}/> */}
 </Routes>
 </Router>
 
